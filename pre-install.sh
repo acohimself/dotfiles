0e96b5edb7da015e44a4aa767c1b17903007e7cb
Andreas Olsen <andreas@tradingdesk.dk>
2021-03-29 17:30:22 +0200

    fixed path to home dir. Install vim-plug

--- pre-install.sh
+++ pre-install.sh
@@ -10,3 +10,10 @@ if [ ! -d "$OMZDIR" ]; then
 	$OMZDIR/tools/upgrade.sh
 fi
 
+
+# Install vim-plug
+if [! -f "$HOME/.vim/autoload/plug.vim" ]; then
+	echo 'Installing vim-plug'
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
