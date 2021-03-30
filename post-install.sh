aba4d4714a35426a8cd32d3c80fa8d446d2ab9fc
Andreas Olsen <andreas@tradingdesk.dk>
2021-03-30 10:29:59 +0200

    bullet-train theme

---
+++ post-install.sh
@@ -0,0 +1,3 @@
+# Install vim-plug
+echo 'Installing plugins'
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"                                                                                                                                                                     
