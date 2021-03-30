6723fbd762caf17a134865ac8ff4836fef8d95ea
Andreas Christian Olsen <aco@acohimself.com>
2021-03-30 10:10:54 +0200

    force links, change theme

--- pre-install.sh
+++ pre-install.sh
@@ -2,9 +2,10 @@
 
 # Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
+echo $OMZDIR
 if [ ! -d "$OMZDIR" ]; then
       echo 'Installing oh-my-zsh'
-        /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+      /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     else
 	echo 'Updating oh-my-zsh'
 	$OMZDIR/tools/upgrade.sh
