5c803013a4f2298c7ed78e2f9764889779827a1d
Andreas Christian Olsen <aco@acohimself.com>
2025-09-17 10:55:01 +0200

    Fish config with auto-bootstrap

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,38 @@
+if status is-interactive
+    # Enable vi key bindings
+    fish_vi_key_bindings
+
+    # Customize vi mode cursor
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual block
+end
+
+# Environment variables
+set -gx EDITOR nvim
+set -gx TERM xterm-256color
+set -gx LSCOLORS "exgxfxdacxDaDaxbadacex"
+
+# Path additions
+fish_add_path ~/bin
+fish_add_path /usr/local/bin
+fish_add_path ~/Library/Python/3.8/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path ~/.npm-global/bin
+
+# Aliases
+alias vi=nvim
+alias dc="docker compose"
+alias domce="ssh root@ssh.domce.dk"
+alias get-dev-ip='aws ec2 describe-instances --output=text --region=eu-central-1 --filters Name="tag:Name",Values="ECS - dev" --query "Reservations[*].Instances[*].PublicIpAddress"'
+
+# Initialize tools
+if type -q direnv
+    direnv hook fish | source
+end
+
+# FZF integration
+if test -f ~/.config/fish/functions/fzf_key_bindings.fish
+    source ~/.config/fish/functions/fzf_key_bindings.fish
+end
